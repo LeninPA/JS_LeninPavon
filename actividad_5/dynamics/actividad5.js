8f23dbcadd7680579493f9b721536e9f493bad48
LeninPA <lenin.pavon@comunidad.unam.mx>
2020-06-19 13:33:41 -0500

    FEAT: Actividad 5

--- actividad_5/dynamics/actividad5.js
+++ actividad_5/dynamics/actividad5.js
@@ -1,9 +1,24 @@
 //Recepción de datos
-// var num = prompt("Ingrese un número");
-let texto= new String("<script>equisdeequisdeequisdeequisdeequisde</script>");
+let texto = prompt("Ingrese un texto en este sitio completamente indefenso que no es una trampa");
 
 let numScript = texto.match(/<script>.*<\/script>/);
 let numEquisde = texto.match(/equisde/g);
 
+if (numScript == null){
+  numScript =  new Array();
+}
+if (numEquisde == null) {
+  numEquisde = new Array();
+}
+
+if (numScript.length == 1 && numEquisde.length == 5){
+  alert("Oso oso mentiroso, vete lejos antes de que te lanze mis calcetines olor a queso.");
+}
+else if (numScript.length == 1 || numEquisde.length == 5){
+  alert("¿Bob?\n ¿Wade?")
+}
+else{
+  alert("¿Cómo encontraste este sitio?\nSal de aquí")
+}
 console.log(numScript.length);
 console.log(numEquisde.length);
