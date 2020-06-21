d9260e86900152a0603a9a9e8c8a3fce74b865ee
LeninPA <lenin.pavon@comunidad.unam.mx>
2020-06-20 20:34:57 -0500

    FEAT: Actividad 4

--- actividad_4/dynamics/actividad4.js
+++ actividad_4/dynamics/actividad4.js
@@ -1,23 +1,33 @@
-//Recepción de datos
+/**
+ * Este programa toma un número mayor entero a 1 y menor a 100 lo descompone en 
+ * sus factores primos únicos. Es decir, que si se ingresan 6 o 12, se obtendrá 
+ * el mismo resultado: 2 y 3.
+ */
+// Recepción de datos
 var num = prompt("Ingrese un número");
+// Verificación de datos
 if (num <= 1) {
   console.log("Ingrese un número mayor a uno")
 }
 else {
+  // Creación de primos
   var primos = [
     2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 53, 59, 67, 71, 73, 79, 83, 89, 97
   ];
   var divisores = [];
   for (valor of primos) {
     if (valor<=num) {
+      // Verifica la divisibilidad
       let div = num%valor;
       if (div == 0) {
         divisores.push(valor);
       }
     }
     else {
+      // Si un primo es mayor al número, se rompe el recorrido, porque ya no hay más primos
       break;
     }
   }
+  // Imprime los resultados
   console.log(divisores);
 }
